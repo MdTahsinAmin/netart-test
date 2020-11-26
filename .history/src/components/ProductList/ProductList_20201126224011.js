23828808e3696b0dfdcecfb44c27287a10912cac
Your NameMdTahsinAmin <Tahsin16@cse.pstu.ac.bd.com>
2020-11-26 23:07:36 +0600

    some

---
+++ .history/src/components/ProductList/ProductList_20201126224011.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ProductList.css';
+const ProductList = () => {
+
+    const style ={
+          textTransform:'uppercase',
+          fontSize:'15px',
+          fontWeight:'900',
+          textAlign:'center'
+    };
+    return (
+        <div className='company-product'>
+            <p style={style}><strong>C.R.I fluid system products cate to diverse segments.</strong></p>
+            <p className='company-services'>Chemical &amp; Process <span>|</span> Power <span>|</span> Water &amp; Waste Water <span>|</span> Oils &amp; Gas <span>|</span> Pharma <span>|</span> Sugars &amp; Distilleries <span>|</span> Paper &amp; Pulp <span>|</span> Marine &amp; defence <span>|</span> Metal &amp; mining <span>|</span> Food &amp; beverage <span>|</span></p>
+            <p className='company-services-1'>Petrochemical &amp; Refineries <span>|</span> Solar <span>|</span> Building <span>|</span> Hvac <span>|</span> Fire Fighting <span>|</span> Agriculture &amp; Residential.</p>
+        </div>
+    );
+};
+
+export default ProductList;
+
+//<p className='company-services-1'>
