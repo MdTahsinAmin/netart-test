1558a9b88e804413589cfcda057c1bd892ffd62c
Your NameMdTahsinAmin <Tahsin16@cse.pstu.ac.bd.com>
2020-11-26 21:55:05 +0600

    background-color added

--- .history/src/components/Product/Product_20201126214905.js
+++ .history/src/components/Product/Product_20201126214905.js
@@ -3,7 +3,7 @@ import './Product.css';
 import products from '../../images/product-list.png';
 const Product = () => {
     return (
-    <div className='section'>
+    <div>
      <p className='product-description'>Installed over 10 lakhs star rated pumpsets across the country resulting in a cumulative saving of more then 900 million units of power for the nation.</p>
      <div className='product-container'>
           <div className='product-list-img'>
